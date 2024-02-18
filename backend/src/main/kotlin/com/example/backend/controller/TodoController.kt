6fb8982bdddf515b778e95bfb63995ade396ee49
yusuke nosaka <yusuke_nosaka@jp.honda>
2024-02-18 11:34:34 +0900

    Add hint

--- backend/src/main/kotlin/com/example/backend/controller/TodoController.kt
+++ backend/src/main/kotlin/com/example/backend/controller/TodoController.kt
@@ -1,22 +1,37 @@
 package com.example.backend.controller
 
 import com.example.backend.domain.Todo
+import com.example.backend.repository.TodoRepository
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class TodoController {
-    private val defaultTodos = listOf(
-        Todo(1, "Hello"),
-        Todo(2, "World"),
-        Todo(3, "Honda"),
-        Todo(4, "Tanzu")
-    )
+class TodoController(private val todoRepository: TodoRepository) {
+//    // setup database,
+//    // not h2
+//    // install postgres
+//    // install docker deskop
+//    // this is memory, not database
+//    private val defaultTodos = listOf(
+//        Todo(1, "Hello"),
+//        Todo(2, "World"),
+//        Todo(3, "Honda"),
+//        Todo(4, "Tanzu")
+//    )
 
     @GetMapping( "/todos")
     @CrossOrigin
     fun getTodoList(): List<String> {
-        return defaultTodos.map { it.task }
+        return todoRepository.findAll()
+            .map { it.task }
     }
+    // today study
+    // what do you learn today?
+    // 1. create database
+    // 2. create table
+    // 3. insert data
+    // 4. different h2(embedded database for test) and real database
+    // 5. library add (maven site)
+    // 6. backend connect to database
 }
---
+++ backend/src/main/kotlin/com/example/backend/repository/TodoRepository.kt
@@ -0,0 +1,7 @@
+package com.example.backend.repository
+
+import com.example.backend.domain.TodoEntity
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface TodoRepository: JpaRepository<TodoEntity, Long> {
+}
